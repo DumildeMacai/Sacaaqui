290471da5facf8df8160d3683f6c3005ebed5257
Dumilde Neto Macai <dumildemacai@gmail.com>
2025-08-11 09:38:52 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['pt-BR', 'en-US'], // Defina os idiomas suportados
-    defaultLocale: 'pt-BR', // Defina o idioma padrão
-  },
   // ... outras configurações
 };
 
