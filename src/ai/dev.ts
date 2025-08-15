673a6a9a09a9eacd043c9ff50c5cce39c0e30be2
Dumilde Neto Macai <dumildemacai@gmail.com>
2025-08-15 17:11:12 +0000

    Error: AI verification failed: [GoogleGenerativeAI Error]: Error fetchin

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,11 +1,15 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import 'dotenv/config';
 
-export const ai = genkit({
+import { genkit } from 'genkit';
+import {googleAI} from '@genkit-ai/googleai';
+import {startDevelopmentFlowServer} from '@genkit-ai/next/dev';
+
+genkit({
   plugins: [
     googleAI({
       apiKey: process.env.GEMINI_API_KEY,
     }),
+    startDevelopmentFlowServer(),
   ],
   logLevel: 'debug',
   model: 'googleai/gemini-2.0-flash',
